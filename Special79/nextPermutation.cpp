6fab11ad64ae59809c9c6ea6d5c1ea910905b0d8
atharvguptaaa <workinatharv@gmail.com>
2024-08-07 00:20:08 +0530

    c

---
+++ Special79/nextPermutation.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> nextPermutation(vector<int> &nums){
+int n=nums.size();
+int ind=-1;
+for(int i=n-2;i>=0;i--){
+    if(nums[i]<nums[i+1]){
+        ind=i;
+        break;
+    }
+}
+if(ind==-1){
+    reverse(nums.begin(),nums.end());
+    return nums;
+}
+
+for(int i=n-1;i>=ind+1;i--){
+    if(nums[i]>nums[ind]){
+    swap(nums[i],nums[ind]);
+    break;
+    }
+}
+reverse(nums.begin()+ind+1,nums.end());
+return nums;
+}
+
+
+int main()
+{
+ vector<int> nums={2,1,5,4,3,0,0};
+ nums=nextPermutation(nums);
+ for(auto it:nums ){
+    cout<<it<<" ";
+ }
+ return 0;
+}
