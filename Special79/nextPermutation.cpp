dc62acbfb94e48413db5f22505b319f9a3804d3f
atharvguptaaa <workinatharv@gmail.com>
2024-08-14 22:45:36 +0530

    c

--- Special79/nextPermutation.cpp
+++ Special79/nextPermutation.cpp
@@ -4,7 +4,7 @@ using namespace std;
 vector<int> nextPermutation(vector<int> &nums){
 int n=nums.size();
 int ind=-1;
-for(int i=n-2;i>=0;i--){               //find breakpoint
+for(int i=n-2;i>=0;i--){               //find breakpoint.
     if(nums[i]<nums[i+1]){
         ind=i;
         break;
@@ -23,7 +23,7 @@ for(int i=n-1;i>=ind+1;i--){           //if found, swap it with smallest no. to 
 }
 reverse(nums.begin()+ind+1,nums.end());   //reverse the array to the right of ind to sort it
 return nums;
-}
+} 
 
 
 int main()
